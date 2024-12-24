f4d3e85e89c39364e4cc0bee320e783ef8158d40
Sondos Sami <samysnds99@gmail.com>
2024-12-24 21:57:59 +0200

    Initial commit

---
+++ Greedy/Q3.cpp
@@ -0,0 +1,3 @@
+//
+// Created by Dell on 24/12/2024.
+//
---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "DP/Q1.cpp"
+#include "DP/Q2.cpp"
+#include "DP/Q3.cpp"
+#include "DP/Q4.cpp"
+#include "DP/Q5.cpp"
+#include "Greedy/Q1.cpp"
+#include "Greedy/Q2.cpp"
+#include "Greedy/Q3.cpp"
+#include "Greedy/Q4.cpp"
+#include "Greedy/Q5.cpp"
+using namespace std;
+
+
+int main() {
+
+}
