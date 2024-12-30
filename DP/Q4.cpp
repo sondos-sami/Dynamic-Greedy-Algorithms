34a2b6a1605ac266a2a2d41f03dad46dc0d01bf8
Marwa Haron <115185549+marwaharon27@users.noreply.github.com>
2024-12-30 23:19:08 +0200

    Update Q4.cpp

--- DP/Q4.cpp
+++ DP/Q4.cpp
@@ -1,3 +1,32 @@
-//
-// Created by Dell on 24/12/2024.
-//
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+struct Treasure {
+    int depth;
+    int gold;
+};
+
+pair<int, vector<int>> calculateMaxGold(int t, int w, const vector<Treasure>& treasures) {
+    int n = treasures.size();
+    vector<int> dp(t + 1, 0);
+    vector<vector<int>> choices(t + 1);
+
+    for (int i = 0; i < n; ++i) {
+        int total_time = 3 * w * treasures[i].depth;
+
+        for (int j = t; j >= total_time; --j) {
+            if (dp[j] < dp[j - total_time] + treasures[i].gold) {
+                dp[j] = dp[j - total_time] + treasures[i].gold;
+                choices[j] = choices[j - total_time];
+                choices[j].push_back(i);
+            }
+        }
+    }
+
+    int max_gold = *max_element(dp.begin(), dp.end());
+    int best_time = max_element(dp.begin(), dp.end()) - dp.begin();
+
+    return {max_gold, choices[best_time]};
+}
