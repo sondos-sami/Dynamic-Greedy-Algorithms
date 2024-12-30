454e0d478503554a21532e9c6716a7afe7a03e35
Marwa Haron <115185549+marwaharon27@users.noreply.github.com>
2024-12-30 23:19:28 +0200

    Update Q1.cpp

--- Greedy/Q1.cpp
+++ Greedy/Q1.cpp
@@ -1,3 +1,19 @@
-//
-// Created by Dell on 24/12/2024.
-//
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int findContentChildren(vector<int>& g,vector<int>& s) {
+   sort(g.begin(), g.end());
+    sort(s.begin(), s.end());
+
+    int childIndex = 0;
+    int cookieIndex = 0;
+    while (childIndex < g.size() && cookieIndex < s.size()) {
+        if (s[cookieIndex] >= g[childIndex]) {
+            childIndex++;
+        }
+        cookieIndex++;
+    }
+    return childIndex;
+}
+
